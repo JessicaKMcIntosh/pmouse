1bc2360f68529c387f9f1e446747d56187f06e01
Jessica McIntosh <JessicaKMcIntosh@gmail.com>
2021-03-23 19:49:07 -0700

    Added more tests and examples.

--- src/testexamples.pp
+++ src/testexamples.pp
@@ -21,13 +21,14 @@ unit testexamples;
 
 interface
 
-uses fpcunit, testregistry, consoletestrunner, Process;
+uses fpcunit, testregistry, consoletestrunner, Process, sysutils;
 
 type
   TTestExamples = class(TTestCase)
   published
     Procedure test_mou;
-
+    Procedure mcdonald_mou;
+    Procedure bottles_mou;
   end;
 
 const
@@ -50,15 +51,124 @@ const
     'Scope of Variables:  E: 1000=1000' + LineEnding +
     'Recursive Macros:  G: 2 3 3 5 7 ' + LineEnding +
     'End of tests.' + LineEnding;
+  mcdonald_mou_output =
+    '' + LineEnding +
+    '' + LineEnding +
+    'How many verses? ' + LineEnding +
+    '' + LineEnding +
+    'Old MacDonald had a farm, ee-igh, ee-igh, oh,' + LineEnding +
+    'And on this farm he had some chicks, ee-igh, ee-igh, oh,' + LineEnding +
+    'With a Chick Chick here and a Chick Chick there,' + LineEnding +
+    'Here a Chick,there a Chick,everywhere a Chick Chick,' + LineEnding +
+    'Old MacDonald had a farm, ee-igh, ee-igh, oh.' + LineEnding +
+    '' + LineEnding +
+    'Old MacDonald had a farm, ee-igh, ee-igh, oh,' + LineEnding +
+    'And on this farm he had some ducks, ee-igh, ee-igh, oh,' + LineEnding +
+    'With a Quack Quack here and a Quack Quack there,' + LineEnding +
+    'Here a Quack,there a Quack,everywhere a Quack Quack,' + LineEnding +
+    'Chick Chick here and a Chick Chick there,' + LineEnding +
+    'Here a Chick,there a Chick,everywhere a Chick Chick,' + LineEnding +
+    'Old MacDonald had a farm, ee-igh, ee-igh, oh.' + LineEnding +
+    '' + LineEnding +
+    'How many verses? ';
+  bottles_mou_output =
+    'HOW MANY VERSES? TWO GREEN BOTTLES STANDING ON THE WALL,' + LineEnding +
+    'TWO GREEN BOTTLES;' + LineEnding +
+    'IF ONE OF THOSE BOTTLES SHOULD HAPPEN TO FALL,' + LineEnding +
+    'ONE GREEN BOTTLES STANDING ON THE WALL.' + LineEnding +
+    '' + LineEnding +
+    'ONE GREEN BOTTLES STANDING ON THE WALL,' + LineEnding +
+    'ONE GREEN BOTTLES;' + LineEnding +
+    'IF ONE OF THOSE BOTTLES SHOULD HAPPEN TO FALL,' + LineEnding +
+    'NO GREEN BOTTLES STANDING ON THE WALL.' + LineEnding +
+    '' + LineEnding +
+    'HOW MANY VERSES? ';
 
 implementation
 
+{ A test program that exercises various bits of standard mouse. }
 Procedure TTestExamples.test_mou;
 var
   output: string;
 begin
   RunCommand('pmouse',['examples' + DirectorySeparator + 'test.mou'],output);
-  AssertEquals('Testing "test.mou".', output, test_mou_output);
+  AssertEquals('Testing "test.mou".', test_mou_output, output);
+end;
+
+{ Execute a process that takes input and return the output. }
+function RunProcessWithInput(executable, parameter: string; input: array of string) : string;
+var
+  TestProcess: TProcess;
+  Buffer: array[0..1024] of char;
+  ReadCount: Integer;
+  ReadSize: Integer;
+  item: string;
+begin
+  { Setup the process. }
+  TestProcess            := TProcess.Create(nil);
+  TestProcess.Options    := [poUsePipes,poStderrToOutPut];
+  TestProcess.Executable := executable;
+  TestProcess.Parameters.Add(parameter);
+  TestProcess.Execute;
+
+  { Send the input to the program. }
+  for item in input do
+  begin
+    Buffer :=  item + LineEnding;
+    TestProcess.Input.Write(Buffer, length(Buffer));
+  end;
+  TestProcess.CloseInput;
+
+  { Wait for the process to finish. }
+  while TestProcess.Running do
+    Sleep(1);
+
+  { Read the output. }
+  ReadSize := TestProcess.Output.NumBytesAvailable;
+  if ReadSize > SizeOf(Buffer) then
+    ReadSize := SizeOf(Buffer);
+  if ReadSize > 0 then
+  begin
+    ReadCount := TestProcess.Output.Read(Buffer, ReadSize);
+    WriteLn(Copy(Buffer,0, ReadCount));
+  end
+  else
+    Buffer := '';
+
+  { Free the process. }
+  TestProcess.Free;
+
+  { Return the output. }
+  RunProcessWithInput := Buffer;
+
+end;
+
+{
+  The example mcdonald.mou asks for input.
+  Without input it will hang.
+  This is the 'Old MacDonald' song.
+}
+Procedure TTestExamples.mcdonald_mou;
+var
+  output: string;
+  input: array [1..2] of string = ('2', '0');
+begin
+  output := RunProcessWithInput('pmouse', 'examples/mcdonald.mou', input);
+  AssertEquals('Testing "mcdonald.mou".', mcdonald_mou_output, output);
+end;
+
+{
+  The example mcdonald.mou asks for input.
+  Without input it will hang.
+  This is a common variation of the 'Bottles on the wall' song.
+}
+Procedure TTestExamples.bottles_mou;
+var
+  output: string;
+  input: array [1..2] of string = ('2', '0');
+begin
+  output := RunProcessWithInput('pmouse', 'examples/bottles.mou', input);
+  AssertEquals('Testing "bottles.mou".', bottles_mou_output, output);
 end;
 
 end.
--- src/testtests.pp
+++ src/testtests.pp
@@ -27,7 +27,8 @@ type
   TTestTests = class(TTestCase)
   published
     Procedure testelse_mou;
-
+    Procedure testtrc_mou;
+    Procedure testfor_mou;
   end;
 
 const
@@ -40,15 +41,52 @@ const
     'OK-12  OK-13  ' + LineEnding +
     'OK-14  OK-15  OK-16  OK-17  ' + LineEnding +
     'OK-18  OK-19  OK-20  OK-21  OK-22  OK-23  OK-24  OK-25  ' + LineEnding;
+    testtrc_mou_output =
+      '                                      {1 2 3++!}$                              ' + LineEnding +
+      '                                       ^ SP: 0 Stack: NULL' + LineEnding +
+      '                                    {1 2 3++!}$                                ' + LineEnding +
+      '                                       ^ SP: 1 Stack: 1' + LineEnding +
+      '                                  {1 2 3++!}$                                  ' + LineEnding +
+      '                                       ^ SP: 2 Stack: 2, 1' + LineEnding +
+      '                                 {1 2 3++!}$                                   ' + LineEnding +
+      '                                       ^ SP: 3 Stack: 3, 2, 1' + LineEnding +
+      '                                {1 2 3++!}$                                    ' + LineEnding +
+      '                                       ^ SP: 2 Stack: 5, 1' + LineEnding +
+      '                               {1 2 3++!}$                                     ' + LineEnding +
+      '                                       ^ SP: 1 Stack: 6' + LineEnding +
+      '6                              {1 2 3++!}$                                      ' + LineEnding +
+      '                                       ^ SP: 0 Stack: NULL' + LineEnding;
+    testfor_mou_output =
+      '1  2  3  4  5  6  7  8  9  10  ' + LineEnding +
+      '10  9  8  7  6  5  4  3  2  1  ' + LineEnding;
 
 implementation
 
+{ Tests the new else statement that David G. Simpson came up with. }
 Procedure TTestTests.testelse_mou;
 var
   output: string;
 begin
   RunCommand('pmouse',['tests' + DirectorySeparator + 'testelse.mou'],output);
-  AssertEquals('Testing "testelse.mou".', output, testelse_mou_output);
+  AssertEquals('Testing "testelse.mou".', testelse_mou_output, output);
+end;
+
+{ Tests the trace functionality. }
+Procedure TTestTests.testtrc_mou;
+var
+  output: string;
+begin
+  RunCommand('pmouse',['tests' + DirectorySeparator + 'testtrc.mou'],output);
+  AssertEquals('Testing "testtrc.mou".', testtrc_mou_output, output);
+end;
+
+{ Tests for loops. }
+Procedure TTestTests.testfor_mou;
+var
+  output: string;
+begin
+  RunCommand('pmouse',['tests' + DirectorySeparator + 'testfor.mou'],output);
+  AssertEquals('Testing "testfor.mou".', testfor_mou_output, output);
 end;
 
 end.
